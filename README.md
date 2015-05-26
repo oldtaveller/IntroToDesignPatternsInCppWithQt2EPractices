62f83516c073dcc3f9754daebb9101134d5aa6d5
myd7349 <myd7349@gmail.com>
2015-05-26 17:56:14 +0800

    Add example 1.13

--- README.md
+++ README.md
@@ -14,15 +14,15 @@ Resources:
         - 1.4 Standard Input and Output
             - EXAMPLE 1.2 [src/iostream/io.cpp](src/iostream/io.cpp) P9
             - 1.4.1 Exercises P10
-                1. `none`
-                2.  [exercises/e1.4.1.2.cpp](exercises/e1.4.1.2.cpp)
+                - 1. `none`
+                - 2.  [exercises/e1.4.1.2.cpp](exercises/e1.4.1.2.cpp)
         - 1.5 Introduction to Functions
             - EXAMPLE 1.3 [src/early-examples/fac2.cpp](src/early-examples/fac2.cpp) P12
             - EXAMPLE 1.4 [src/functions/overload-not.cpp](src/functions/overload-not.cpp) P15
             - EXAMPLE 1.5 [src/functions/overload.cpp](src/functions/overload.cpp) P16
             - 1.5.1 Exercises P17
-                1. [exercises/e1.5.1.1.cpp](exercises/e1.5.1.1.cpp)
-                2. [exercises/e1.5.1.2.cpp](exercises/e1.5.1.2.cpp)
+                - 1. [exercises/e1.5.1.1.cpp](exercises/e1.5.1.1.cpp)
+                - 2. [exercises/e1.5.1.2.cpp](exercises/e1.5.1.2.cpp) *TODO*
         - 1.6 qmake, Project Files, and Makefile
             - EXAMPLE 1.6 [src/common.pri](src/common.pri) P21
         - 1.7 Getting Help Online
@@ -32,6 +32,11 @@ Resources:
         - 1.9 Streams
             - EXAMPLE 1.9 [src/stdstreams/streamdemo.cpp](src/stdstreams/streamdemo.cpp) P27
             - EXAMPLE 1.10 [src/qtstreams/qtstreamdemo.cpp](src/qtstreams/qtstreamdemo.cpp) P28
+        - 1.10 File Streams
+            - EXAMPLE 1.11 [src/stl/streams/streams.cpp](src/stl/streams/streams.cpp) P29
+            - EXAMPLE 1.12 [src/stl/streams/streams.cpp](src/stl/streams/streams.cpp) P30
+            - EXAMPLE 1.13 [src/stl/streams/streams.cpp](src/stl/streams/streams.cpp) P31
+            - EXAMPLE 1.14 [src/qtstreams/files/qdemo.cpp](src/qtstreams/files/qdemo.cpp) P31
     - Chapter 2: Top of the class
     - Chapter 3: Introduction to Qt
     - Chapter 4: Lists
---
+++ src/stl/streams/streams.cpp
@@ -0,0 +1,85 @@
+// 2015-05-26T17:27+08:00
+// P29: Example 1.11 1.12 1.13
+// This example creates some strings from characters and numerics and writes
+// them to a file.
+#include <ciso646>
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+int main()
+{
+    // ---- Example 1.11
+    std::ostringstream strbuf;
+
+    int lucky = 7;
+    float pi = 3.14f;
+    double e = 2.71;
+
+    std::cout << "An in-memory stream" << std::endl;
+    strbuf << "luckynumber: " << lucky << std::endl
+           << "pi: " << pi << std::endl
+           << "e: " << e << std::endl;
+
+    // Converts the stringstream to string
+    std::string strval = strbuf.str();
+    std::cout << strval;
+
+    std::ofstream outf; // An output file stream
+    outf.open("mydata"); // Creates (or overwrites) a disk file for output 
+    outf << strval;
+    outf.close();
+
+    std::cout << "Read data from the file - watch for errors." << std::endl;
+
+    // ---- Example 1.12
+    std::string newstr;
+    std::ifstream inf; // An input file stream
+    
+    inf.open("mydata");
+    if (inf) { // Make sure the file exists before attempting to read
+        int lucky2;
+        inf >> newstr >> lucky2;
+        if (lucky != lucky2) {
+            std::cerr << "ERROR! Wrong " << newstr << lucky2 << std::endl;
+        } else {
+            std::cout << newstr << " OK" << std::endl;
+        }
+
+        float pi2;
+        inf >> newstr >> pi2;
+        if (pi2 != pi) {
+            std::cerr << "ERROR! Wrong " << newstr << pi2 << std::endl;
+        } else {
+            std::cout << newstr << " OK" << std::endl;
+        }
+
+        double e2;
+        inf >> newstr >> e2;
+        if (e2 != e) {
+            std::cerr << "ERROR! Wrong " << newstr << e2 << std::endl;
+        } else {
+            std::cout << newstr << " OK" << std::endl;
+        }
+
+        inf.close();
+    }
+
+    // Example 1.13
+    std::cout << "Read from file line-by-line" << std::endl;
+    inf.open("mydata");
+    if (inf) {
+        while (not inf.eof()) {
+            std::getline(inf, newstr);
+            std::cout << newstr << std::endl;
+        }
+
+        inf.close();
+    }
+
+    std::remove("mydata");
+
+    return 0; 
+}
